2e72f6f80f174371ec300c38536a25c28348d136
Michael Mrozek <git@mrozekma.com>
2017-11-05 19:24:14 -0500

    Logo

--- src/main/java/com/mrozekma/atlassian/bitbucket/projectFields/ao/CustomField.java
+++ src/main/java/com/mrozekma/atlassian/bitbucket/projectFields/ao/CustomField.java
@@ -22,8 +22,6 @@ public interface CustomField extends Entity {
     String getOptions();
     void setOptions(String options);
 
-    //TODO Global settings UI for these:
-
     @Accessor("visible")
     boolean isVisibleInProjectList();
     @Mutator("visible")

8891de0b22871488992ed72898165118ee601396
Michael Mrozek <git@mrozekma.com>
2018-08-20 23:57:13 -0400

    Update field values when a project's key changes

--- src/main/java/com/mrozekma/atlassian/bitbucket/projectFields/impl/ProjectModifyDeleteListener.java
+++ src/main/java/com/mrozekma/atlassian/bitbucket/projectFields/impl/ProjectModifyDeleteListener.java
@@ -2,6 +2,7 @@ package com.mrozekma.atlassian.bitbucket.projectFields.impl;
 
 import com.atlassian.activeobjects.external.ActiveObjects;
 import com.atlassian.bitbucket.event.project.ProjectDeletedEvent;
+import com.atlassian.bitbucket.event.project.ProjectModifiedEvent;
 import com.atlassian.event.api.EventListener;
 import com.atlassian.plugin.spring.scanner.annotation.imports.ComponentImport;
 import com.mrozekma.atlassian.bitbucket.projectFields.ao.CustomFieldValue;
@@ -9,13 +10,27 @@ import net.java.ao.Query;
 
 import javax.inject.Named;
 
-@Named("projectDeleteListener")
-public class ProjectDeleteListener {
+@Named("projectModifyDeleteListener")
+public class ProjectModifyDeleteListener {
     @ComponentImport
     private final ActiveObjects activeObjects;
 
-    public ProjectDeleteListener(ActiveObjects activeObjects) {
+    public ProjectModifyDeleteListener(ActiveObjects activeObjects) {
         this.activeObjects = activeObjects;
+    }
+
+    @EventListener
+    public void onProjectModified(ProjectModifiedEvent e) {
+        final String oldKey = e.getOldValue().getKey(), newKey = e.getNewValue().getKey();
+        if(!oldKey.equals(newKey)) {
+            this.activeObjects.executeInTransaction(() -> {
+                for(CustomFieldValue val : this.activeObjects.find(CustomFieldValue.class, Query.select().where("PROJECT_KEY = ?", oldKey))) {
+                    val.setProjectKey(newKey);
+                    val.save();
+                }
+                return null;
+            });
+        }
     }
 
     @EventListener
